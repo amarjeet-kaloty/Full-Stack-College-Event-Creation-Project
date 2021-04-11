579342abc5821707c76875c61ca652b2e9b4e56d
tommygreco <65359900+tommygreco@users.noreply.github.com>
2021-04-10 20:23:32 -0400

    Spring Break Accessibility
    Pushing current progress to this branch so I can access code from laptop while on spring break.

---
+++ college-event/Classes/EventStream.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Xml;
+
+namespace college_event.Classes
+{
+    public class EventXML
+    {
+        public String id;
+        public String title;
+        public String startDate;
+        public String startTime;
+        public String endDate;
+        public String endTime;
+        public String location;
+        public String description;
+        public String url;
+
+        public EventXML()
+        {
+
+        }
+    }
+
+    public class EventStream
+    {
+        XmlTextReader reader;
+        public List<EventXML> list = new List<EventXML>();
+        EventXML temp;
+
+        public EventStream(String url)
+        {
+            temp = new EventXML();
+            reader = new XmlTextReader(url);
+            while (reader.Read())
+            {
+                switch(reader.NodeType)
+                {
+                    case XmlNodeType.EndElement:
+                        if (reader.Name.Equals("event"))
+                        {
+                            list.Add(temp);
+                            temp = new EventXML();
+                        }
+                        break;
+                    case XmlNodeType.Element:
+                        if (reader.Name.Equals("calendar"))
+                            break;
+                        else if (reader.Name.Equals("id"))
+                        {
+                            reader.Read();
+                            temp.id = reader.Value;
+                        }
+                        else if (reader.Name.Equals("start_date"))
+                        {
+                            reader.Read();
+                            temp.startDate = reader.Value;
+                            //.Substring(5, 16);
+                            temp.startTime = reader.Value;
+                                //.Substring(17, 25);
+                        }
+                        else if (reader.Name.Equals("end_date"))
+                        {
+                            reader.Read();
+                            temp.endDate = reader.Value;
+                            //.Substring(5, 16);
+                            temp.endTime = reader.Value;
+                                //.Substring(17, 25);
+                        }
+                        else if (reader.Name.Equals("location"))
+                        {
+                            reader.Read();
+                            temp.location = reader.Value;
+                        }
+                        else if (reader.Name.Equals("title"))
+                        {
+                            reader.Read();
+                            temp.title = reader.Value;
+                        }
+                        else if (reader.Name.Equals("description"))
+                        {
+                            reader.Read();
+                            switch (reader.NodeType)
+                            { 
+                                case XmlNodeType.CDATA:
+                                    temp.description = HttpUtility.HtmlDecode(reader.Value);
+                                    break;
+                            }
+                        }
+                        else if (reader.Name.Equals("webpage"))
+                        {
+                            reader.Read();
+                            if (reader.Name.Equals("url"))
+                                temp.url = reader.Value;
+                        }
+
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ college-event/home.aspx.cs
@@ -0,0 +1,66 @@
+﻿using college_event.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace college_event
+{
+    public partial class home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //try
+            //{
+            //    if (Session["status"].Equals("1"))    // SuperAdmin
+            //    { 
+
+            //    }
+            //    else if (Session["status"].Equals("2")) // User is Admin
+            //    {
+
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Response.Write("<script>alert('" + ex.Message + "');</script>");
+            //}
+
+            String todayURL = "https://events.ucf.edu/feed.xml";
+            String upcomingURL = "https://events.ucf.edu/upcoming/feed.xml";
+            EventStream es = new EventStream(todayURL);
+
+            ListBox1.DataSource = es.list;
+            ListBox1.DataBind();
+        }
+
+        //Create Event
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("CreateEventPage.aspx");
+        }
+
+        // Create University Profile
+        protected void LinkButton2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("CreateUniversityProfile.aspx");
+        }
+
+        // Request To Create Rso
+        protected void requestToCreateRso_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String todayURL = "https://events.ucf.edu/feed.xml";
+            String upcomingURL = "https://events.ucf.edu/upcoming/feed.xml";
+            EventStream es1 = new EventStream(todayURL);
+            ListBox1.DataSource = es1.list;
+            ListBox1.DataBind();
+        }
+    }
+}
